a3d2f2058dcd2988361d200687d0949c3d865e0e
DIluo1549 <diluo154@gmail.com>
2025-04-10 16:19:59 +0800

    修改中继服务器和key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ddns.154155.xyz"];
+pub const RS_PUB_KEY: &str = "ngMpIiSxVIWHBVKBbY4p8KwVb2oVcEiMWGSqINm2TN0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
